bf4bb338e452cf8fc4470d26aad1249078b44202
Andres Mauricio Quintero Rios <Quintero2273@hotmail.com>
2025-08-08 14:01:45 -0500

    finalizar repo persona

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+
+
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Instanciar
+        Persona personaDocente = new Persona("Darwin", 32);
+        Persona personaEstudiante = new Persona("Cristian", 22);
+
+        personaDocente.mostrarInformacion("Hola");
+        String mensajeInformacion = personaDocente.DevolverInformacion();
+        System.out.println(mensajeInformacion);
+
+        personaEstudiante.mostrarInformacion("Hola");
+        mensajeInformacion = personaEstudiante.DevolverInformacion();
+        System.out.println(mensajeInformacion);
     }
 }
